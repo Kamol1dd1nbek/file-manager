d3103532e177df44cf9def10ef8804dd764da4e1
Kamoliddinbek <kamoliddinbekxudoyberdi@gmail.com>
2024-08-19 11:25:59 +0500

    simple tabs created

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,20 +1,59 @@
 // style
-import { useState } from "react";
 import "./Tabs.css";
+
+import { useState } from "react";
+
+const newTab = {
+  id: 2,
+  icon: "🏠",
+  title: "Desctop",
+  path: "C://usr/desctop"
+}
 
 function Tabs() {
   const [tabs, setTabs] = useState([{
+    id: 1,
+    icon: "🏠",
+    title: "Desctop",
+    path: "C://usr/desctop"
+  }, {
+    id: 2,
+    icon: "🏠",
     title: "Desctop",
     path: "C://usr/desctop"
   }])
-  return <div className="tabs">
-    {tabs && tabs.map(tab => {
-      return <div key={tab.title} className="tabs__tab tabs__tab--active">
-        <span className="tab__title">{tab.title}</span>
-        <span>x</span>
+
+
+
+  function addNewTab() {
+    setTabs(prev => [...prev, newTab])
+  }
+
+  function removeTab(id) {
+    setTabs(prev => prev.filter(tab => tab.id !== id));
+  }
+
+  return (
+    <>
+      <div className="tabs__space"></div>
+      <div className="tabs">
+        {tabs && tabs.map(tab => {
+          return <div key={tab.title} className="tabs__tab tabs__tab--active">
+            <div className="tab__title">
+              {tab.icon && <div className="tab__icon">{tab.icon}</div>}
+              <span className="tab__title">{tab.title}</span>
+            </div>
+            <div onClick={() => removeTab(tab.id)} className="tabs__close-btn">
+              <span>+</span>
+            </div>
+          </div>
+        })}
+        <div onClick={addNewTab} className="tabs__add-btn">
+          +
+        </div>
       </div>
-    })}
-  </div>;
+    </>
+  )
 }
 
 export default Tabs;
